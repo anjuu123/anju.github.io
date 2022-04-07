133c3708c5e8dc471de63a06c26aa02206895e08
Anju Dhungana <anjudhungana28@gmail.com>
2022-04-06 23:10:53 -0400

    bootstrap navbar added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,42 +1,98 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import './Header.css'
-import { Container, Nav,Navbar} from 'react-bootstrap'
+import React, { useState } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import { Link } from "react-router-dom";
 
-export default function Header() {
+function NavBar() {
+  const [expand, updateExpanded] = useState(false);
+  const [navColour, updateNavbar] = useState(false);
+
+  function scrollHandler() {
+    if (window.scrollY >= 20) {
+      updateNavbar(true);
+    } else {
+      updateNavbar(false);
+    }
+  }
+
+  window.addEventListener("scroll", scrollHandler);
+
   return (
-    <div className="header">
-      <Navbar bg="white" expand="md" fixed="top">
-        <Container>
-          <Navbar.Brand className="brand" href="/">
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Link className="navbar-links" to="/">
+    <Navbar
+      expanded={expand}
+      fixed="top"
+      expand="md"
+      className={navColour ? "sticky" : "navbar"}
+    >
+      <Container>
+        <Navbar.Brand href="/" className="d-flex">
+          <h1>Anju</h1>
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          onClick={() => {
+            updateExpanded(expand ? false : "expanded");
+          }}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto" defaultActiveKey="#home">
+            <Nav.Item>
+              <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
                 Home
-              </Link>
+              </Nav.Link>
+            </Nav.Item>
 
-              <Link className="navbar-links" to="/about">
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/about"
+                onClick={() => updateExpanded(false)}
+              >
                 About
-              </Link>
+              </Nav.Link>
+            </Nav.Item>
 
-              <Link className="navbar-links" to="/skills">
-               Skills
-              </Link>
-
-              <Link className="navbar-links" to="/projects">
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/project"
+                onClick={() => updateExpanded(false)}
+              >
                 Projects
-              </Link>
+              </Nav.Link>
+            </Nav.Item>
 
-              <Link className="navbar-links" to="/contact">
-               Contact
-              </Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/resume"
+                onClick={() => updateExpanded(false)}
+              >
+                Resume
+              </Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item>
+              <Nav.Link
+                href="https://blogs.soumya-jit.tech/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Contact
+              </Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item className="fork-btn"></Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
-
 }
+
+export default NavBar;
